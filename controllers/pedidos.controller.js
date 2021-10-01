a1919e0dcc072a3096484e674259d467ea956bc4
discoverysystemspos <elkindanielcastillo@gmail.com>
2021-10-01 06:50:54 -0500

    lan

--- controllers/pedidos.controller.js
+++ controllers/pedidos.controller.js
@@ -11,16 +11,18 @@ const getPedidos = async(req, res = response) => {
     try {
 
         const desde = Number(req.query.desde) || 0;
+        const hasta = Number(req.query.hasta) || 50;
+        const status = req.query.status || true;
 
         const [pedidos, total] = await Promise.all([
 
-            Pedido.find()
+            Pedido.find({ status })
             .populate('client', 'name cedula phone email address city tip')
             .populate('products.product', 'name')
             .populate('user', 'name')
             .sort({ pedido: -1 })
             .skip(desde)
-            .limit(50),
+            .limit(hasta),
 
             Pedido.countDocuments()
         ]);
@@ -94,6 +96,40 @@ const getPedidosClient = async(req, res = response) => {
 =========================================================================*/
 
 /** =====================================================================
+ *  GET PEDIDO ONE
+=========================================================================*/
+const getPedidoOne = async(req, res = response) => {
+
+    try {
+
+        const id = req.params.id;
+
+        const pedido = await Pedido.findById(id)
+            .populate('client', 'name cedula phone email address city tip')
+            .populate('products.product', 'name')
+            .populate('user', 'name');
+
+        res.json({
+            ok: true,
+            pedido
+        });
+
+    } catch (error) {
+
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado, porfavor intente nuevamente'
+        });
+
+    }
+
+};
+/** =====================================================================
+ *  GET PEDIDO ONE
+=========================================================================*/
+
+/** =====================================================================
  *  POST PEDIDO
 =========================================================================*/
 const postPedidos = async(req, res = response) => {
@@ -150,5 +186,6 @@ const postPedidos = async(req, res = response) => {
 module.exports = {
     getPedidos,
     postPedidos,
-    getPedidosClient
+    getPedidosClient,
+    getPedidoOne
 }
--- routes/pedidos.route.js
+++ routes/pedidos.route.js
@@ -9,7 +9,7 @@ const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT, validarClientJWT } = require('../middlewares/validar-jwt');
 
 // CONTROLLER
-const { getPedidos, postPedidos, getPedidosClient } = require('../controllers/pedidos.controller');
+const { getPedidos, postPedidos, getPedidosClient, getPedidoOne } = require('../controllers/pedidos.controller');
 
 const router = Router();
 
@@ -25,6 +25,14 @@ router.get('/', validarJWT, getPedidos);
  *  GET PEDIDOS CLIENT
 =========================================================================*/
 router.get('/client', validarClientJWT, getPedidosClient);
+/** =====================================================================
+ *  GET PEDIDOS CLIENT 
+=========================================================================*/
+
+/** =====================================================================
+ *  GET PEDIDOS CLIENT
+=========================================================================*/
+router.get('/one/:id', validarJWT, getPedidoOne);
 /** =====================================================================
  *  GET PEDIDOS CLIENT
 =========================================================================*/
