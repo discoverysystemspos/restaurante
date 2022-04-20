6fc13bebf1330205c5119fd0014061e0f5285f26
Gilmer Contreras <adgdiux@gmail.com>
2022-04-20 05:44:51 -0400

    admin castitoner

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -164,7 +164,7 @@ const renewClientJWT = async(req, res = response) => {
         const token = await generarClientJWT(cid);
 
         // SEARCH USER
-        const client = await Client.findById(cid, 'email name cid img cedula phone city department address valid credit mayoreo');
+        const client = await Client.findById(cid, 'email name cid img cedula phone city department address valid credit mayoreo contratista');
         // SEARCH USER
 
         res.status(200).json({
