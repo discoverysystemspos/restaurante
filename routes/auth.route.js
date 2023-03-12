5476d8847f4d577814fccb122bf109ac8da77a45
Gilmer Contreras <adgdiux@gmail.com>
2023-03-12 18:59:38 -0500

    Superofertas 9

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 /** =====================================================================
- *  LOGIN ROUTER
+ *  LOGIN ROUTERr
 =========================================================================*/
 const { Router } = require('express');
 const { check } = require('express-validator');
