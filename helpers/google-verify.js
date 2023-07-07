f39b43651a586e607f4b38fc31dd7b9dfbba82e1
Gilmer Contreras <adgdiux@gmail.com>
2023-07-07 18:05:38 -0500

    RDJ Rental

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -2,7 +2,7 @@ const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_ID);
 
 /** =====================================================================
- *  GOOGLE VERIFY
+ *  GOOGLE VERIFY NW
 =========================================================================*/
 const googleVerify = async(token) => {
 
