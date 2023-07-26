9df6978bcf2a30a1200198ca04783762ee7faf32
Gilmer Contreras <adgdiux@gmail.com>
2023-07-26 16:21:23 -0400

    LOCAL

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,7 +8,7 @@ const { generarJWT, generarClientJWT } = require('../helpers/jwt');
 const { googleVerify } = require('../helpers/google-verify');
 
 /** =====================================================================
- *  LOGINN
+ *  LOGIN
 =========================================================================*/
 const login = async(req, res = response) => {
 
