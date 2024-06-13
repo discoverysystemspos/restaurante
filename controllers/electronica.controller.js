432077bd386e3d2e35fd05a366bd3be6e11562a6
Gilmer Contreras <adgdiux@gmail.com>
2024-06-13 15:37:52 -0400

    selectbga

--- controllers/electronica.controller.js
+++ controllers/electronica.controller.js
@@ -3,6 +3,7 @@ const { response } = require('express');
 const https = require("https");
 
 const Invoice = require('../models/invoices.model');
+const Dataico = require('../models/dataico.model');
 
 
 
@@ -22,9 +23,36 @@ const createInvoiceElectronic = async(req, res = response) => {
         const factura = req.params.factura;
         const desde = Number(req.params.desde);
         const prefix = invoiceNew.invoice.numbering.prefix;
-        const number = await Invoice.countDocuments({ electronica: true, prefix: prefix, send: true });
 
-        invoiceNew.invoice.number = (number + 1) + desde;
+        const dataicoDB = await Dataico.find();
+        const dataico = dataicoDB[0];
+
+        invoiceNew.invoice.number = dataico.hasta + 1;
+
+        // POST DATAICO
+        const responseOne = await fetch(`https://api.dataico.com/dataico_api/v2/invoices`, {
+            method: 'POST',
+            body: JSON.stringify(invoiceNew),
+            headers: {
+                "auth-token": token,
+            }
+        }).catch(err => {
+            console.log(err);
+            res.json({
+                ok: true,
+                err
+            });
+        });
+
+        const invoiceOne = await responseOne.json();
+
+        let numero = invoiceOne.errors[0]['error'].toString().split("'")[3];
+        // let numero = error.split("'")[3]
+        invoiceNew.invoice.number = numero;
+
+        // OLD
+        // const number = await Invoice.countDocuments({ electronica: true, prefix: prefix, send: true });
+        // invoiceNew.invoice.number = (number + 1) + desde;
 
         // POST DATAICO
         const response = await fetch(`https://api.dataico.com/dataico_api/v2/invoices`, {
@@ -44,7 +72,7 @@ const createInvoiceElectronic = async(req, res = response) => {
         const status = await response.status;
         const invoice = await response.json();
 
-        if (Number(status) !== 201) {
+        if (Number(status) !== 201 || !invoice.cufe) {
             await Invoice.findByIdAndUpdate(factura, { electronica: true, send: false }, { new: true, useFindAndModify: false });
         } else {
             await Invoice.findByIdAndUpdate(factura, { pdf_url: invoice.pdf_url, cufe: invoice.cufe, uuid: invoice.uuid, number: invoice.number, electronica: true, prefix: prefix, send: true }, { new: true, useFindAndModify: false });
