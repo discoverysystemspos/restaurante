abf9faf688eb522fe710793976c02e81db7e8c78
Gilmer Contreras <adgdiux@gmail.com>
2024-07-12 10:21:17 -0400

    pp104

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -27,7 +27,7 @@ const generarJWT = (uid) => {
         /* The code `jwt.sign(payload, process.env.SECRET_SEED_JWT, { expiresIn: '4h' }, (err, token)
         => { ... })` is generating a JSON Web Token (JWT) using the `jsonwebtoken` library. */
         jwt.sign(payload, process.env.SECRET_SEED_JWT, {
-            expiresIn: '4h'
+            expiresIn: '15h'
         }, (err, token) => {
 
             if (err) {
