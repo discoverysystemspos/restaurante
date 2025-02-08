353904a26e40d00386be634e934f52c7af8d3d48
Gilmer Contreras <ivsmca90@gmail.com>
2025-02-08 09:16:44 -0400

    LOCAL

--- controllers/bascula.controller.js
+++ controllers/bascula.controller.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
+const { ByteLengthParser } = require('@serialport/parser-byte-length');
 
 /** =====================================================================
  *  GET PESO
@@ -13,6 +14,8 @@ const getPeso = async(req, res = response) => {
 
     try {
 
+        const type = req.query.type || '1';
+
         // Configura el puerto serial Nuevo
         const port = new SerialPort({
           path: 'COM1', 
@@ -20,55 +23,123 @@ const getPeso = async(req, res = response) => {
         });
       
         let responseSent = false; // Evita múltiples respuestas
-        const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
-      
-        // Timeout de 10 segundos
-        const timeout = setTimeout(() => {
-          if (!responseSent) {
-            responseSent = true;
-            res.json({ 
-              ok: true, 
-              pesos: 0 
+
+
+        switch (type) {
+          case '1':
+
+            const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+        
+            // Timeout de 10 segundos
+            const timeout = setTimeout(() => {
+              if (!responseSent) {
+                responseSent = true;
+                res.json({ 
+                  ok: true, 
+                  pesos: 0 
+                });
+                port.close();
+              }
+            }, 5000);
+          
+            // Escucha datos del puerto serial
+            parser.on('data', (data) => {
+              if (responseSent) return; // Si ya se respondió, ignora
+          
+              // Extrae números y puntos decimales
+              const numeros = data.replace(/[^0-9.]/g, '');
+              const peso = parseFloat(numeros);
+        
+              console.log('LECTURA: ', peso);
+          
+              // Verifica si es un número válido y mayor a 0
+              if (!isNaN(peso) && peso > 0) {
+                responseSent = true;
+            //console.log('PESO FINAL: ', peso);
+                clearTimeout(timeout); // Cancela el timeout
+                res.json({ 
+                  ok: true, 
+                  pesos: peso.toFixed(3) 
+                });
+                port.close(); // Cierra la conexión
+              }
             });
-            port.close();
-          }
-        }, 5000);
-      
-        // Escucha datos del puerto serial
-        parser.on('data', (data) => {
-          if (responseSent) return; // Si ya se respondió, ignora
-      
-          // Extrae números y puntos decimales
-          const numeros = data.replace(/[^0-9.]/g, '');
-          const peso = parseFloat(numeros);
-    
-          console.log('LECTURA: ', peso);
-      
-          // Verifica si es un número válido y mayor a 0
-          if (!isNaN(peso) && peso > 0) {
-            responseSent = true;
-        //console.log('PESO FINAL: ', peso);
-            clearTimeout(timeout); // Cancela el timeout
-            res.json({ 
-              ok: true, 
-              pesos: peso.toFixed(3) 
+          
+            // Manejo de errores
+            port.on('error', (err) => {
+              if (!responseSent) {
+                responseSent = true;
+                clearTimeout(timeout);
+                res.status(500).json({ 
+                  ok: false, 
+                  msg: 'Error de conexión con la báscula, encienda la bascula, verifica que el cable de comunicacion o si la bascula esta conectada en el puerto correcto' 
+                });
+              }
             });
-            port.close(); // Cierra la conexión
-          }
-        });
-      
-        // Manejo de errores
-        port.on('error', (err) => {
-          if (!responseSent) {
-            responseSent = true;
-            clearTimeout(timeout);
+            
+            break;
+
+          case '2':
+
+            const parser2 = port.pipe(new ByteLengthParser({ length: 24 }));;
+          
+            // Timeout de 10 segundos
+            const timeout2 = setTimeout(() => {
+              if (!responseSent) {
+                responseSent = true;
+                res.json({ 
+                  ok: true, 
+                  pesos: 0 
+                });
+                port.close();
+              }
+            }, 5000);
+          
+            // Escucha datos del puerto serial
+            parser2.on('data', (data) => {
+              if (responseSent) return; // Si ya se respondió, ignora
+          
+              // Extrae números y puntos decimales
+              const numeros = data.toString().replace(/[^0-9.]/g, '');
+              const peso = parseFloat(numeros);
+        
+              console.log('LECTURA: ', peso);
+          
+              // Verifica si es un número válido y mayor a 0
+              if (!isNaN(peso) && peso > 0) {
+                responseSent = true;
+            //console.log('PESO FINAL: ', peso);
+                clearTimeout(timeout2); // Cancela el timeout
+                res.json({ 
+                  ok: true, 
+                  pesos: peso.toFixed(3) 
+                });
+                port.close(); // Cierra la conexión
+              }
+            });
+          
+            // Manejo de errores
+            port.on('error', (err) => {
+              if (!responseSent) {
+                responseSent = true;
+                clearTimeout(timeout2);
+                res.status(500).json({ 
+                  ok: false, 
+                  msg: 'Error de conexión con la báscula, encienda la bascula, verifica que el cable de comunicacion o si la bascula esta conectada en el puerto correcto' 
+                });
+              }
+            });
+            
+
+          break;
+        
+          default:
             res.status(500).json({ 
               ok: false, 
-              msg: 'Error de conexión con la báscula, encienda la bascula, verifica que el cable de comunicacion o si la bascula esta conectada en el puerto correcto' 
+              msg: 'Error el modelo de bascula no es el correcto' 
             });
-          }
-        });
-            
+            break;
+        }   
     
         } catch (error) {
             console.log(error);
