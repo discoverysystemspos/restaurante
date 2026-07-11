ae6fa188a233c70a1e47a1f23c8eadb3d25998c4
gacp90 <ivsmca90@gmail.com>
2026-07-11 13:31:53 -0400

    IP

--- database/config.js
+++ database/config.js
@@ -10,7 +10,10 @@ const dbConection = async() => {
 
     try {
         
-        const connection = await mongoose.connect(process.env.DB_CNN);
+        const connection = await mongoose.connect(process.env.DB_CNN, {
+            useCreateIndex: true,
+            useNewUrlParser: true
+        });
 
         autoIncrement.initialize(connection);
         console.log('DB Online');
